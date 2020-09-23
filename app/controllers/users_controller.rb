874f518e8d1fd6070c02b45e976e6e461ceca81d
Lucas Carvalho <lucascarvalhouibai@gmail.com>
2020-09-23 14:11:02 -0300

    Adding edit and update methods to Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+
+  before_action :set_users, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your user information was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def new
     @user = User.new
   end
@@ -15,6 +30,10 @@ class UsersController < ApplicationController
   end
 
   private 
+
+  def set_users
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
